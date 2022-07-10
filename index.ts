a9ad371f173088b854df56c25cf74a998127af30
peoft <paulo.ostermann.filho@gmail.com>
2022-07-10 12:17:04 -0300

    Validacao de tipos e interfaces

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log('TypeScript');
+function soma(a:number, b:number) {
+    return a + b;
+}
+
+// types - estrutura de dados
+// interfaces - contratos para implementar as classes
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executarRugido(decibeis: number): void;
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (decibeis) => (`${decibeis}dB`),
+}
+
+const felino: IFelino = {
+    nome: 'Leao',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    executarRugido: (decibeis) => (`${decibeis}dB`),
+}
