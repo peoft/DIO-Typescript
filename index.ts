ac35d31e223e5dd5eb4ca699513f1d3b6a38c8b9
peoft <paulo.ostermann.filho@gmail.com>
2022-07-10 12:28:43 -0300

    Uso dos types

--- index.ts
+++ index.ts
@@ -2,28 +2,37 @@ function soma(a:number, b:number) {
     return a + b;
 }
 
-// types - estrutura de dados
+// types - estrutura de dados - usar em junções (&) ou merges (|)
 // interfaces - contratos para implementar as classes
 
 interface IAnimal {
     nome: string;
     tipo: 'terrestre' | 'aquático';
-    executarRugido(decibeis: number): void;
+    domestico: boolean;
 }
 
 interface IFelino extends IAnimal {
     visaoNoturna: boolean;
 }
 
-const animal: IAnimal = {
-    nome: 'Elefante',
-    tipo: 'terrestre',
-    executarRugido: (decibeis) => (`${decibeis}dB`),
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type IDomestico = IFelino | ICanino;
+//type IDomestico = IFelino & ICanino;
+
+const animal: IDomestico = {
+   domestico: true,
+   nome: 'cachorro',
+   porte: 'medio',
+   tipo: 'terrestre',
 }
 
 const felino: IFelino = {
     nome: 'Leao',
     tipo: 'terrestre',
+    domestico: true,
     visaoNoturna: true,
-    executarRugido: (decibeis) => (`${decibeis}dB`),
 }
+
